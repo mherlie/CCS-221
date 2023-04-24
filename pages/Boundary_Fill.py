c74522ec1fd82d4fa7a2c942fccecf1de32ce6bc
Mherlie Joy <126627774+mherlie@users.noreply.github.com>
2023-04-24 23:49:22 +0800

    Update Boundary_Fill.py

--- pages/Boundary_Fill.py
+++ pages/Boundary_Fill.py
@@ -2,43 +2,38 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
-st.header("Boundary Fill Algorithm")
+# initialize the 2D array
+two_d_arr = np.zeros((10,10))
 
-# Define the 2D array with dimensions 10x10
-two_d_arr = np.zeros((10, 10))
-
-# Define the boundary coordinates
-boundary_coords = [(3, 3), (3, 4), (4, 4), (4, 5), (5, 5), (5, 4), (6, 4), (6, 3), (5, 3), (5, 2), (4, 2), (4, 3)]
-
-# Define the fill color
-fill_color = 1
-
-# Define the boundary color
-boundary_color = 2
-
-# Function to perform the boundary fill algorithm
+# function for boundary fill algorithm
 def boundary_fill(x, y, fill_color, boundary_color):
-    if x < 0 or y < 0 or x >= 10 or y >= 10:
-        return
-    if two_d_arr[x][y] == boundary_color or two_d_arr[x][y] == fill_color:
-        return
-    two_d_arr[x][y] = fill_color
-    boundary_fill(x+1, y, fill_color, boundary_color)
-    boundary_fill(x-1, y, fill_color, boundary_color)
-    boundary_fill(x, y+1, fill_color, boundary_color)
-    boundary_fill(x, y-1, fill_color, boundary_color)
+    if two_d_arr[x][y] != boundary_color and two_d_arr[x][y] != fill_color:
+        two_d_arr[x][y] = fill_color
+        boundary_fill(x-1, y, fill_color, boundary_color)
+        boundary_fill(x+1, y, fill_color, boundary_color)
+        boundary_fill(x, y-1, fill_color, boundary_color)
+        boundary_fill(x, y+1, fill_color, boundary_color)
 
-# Perform the boundary fill algorithm for each boundary coordinate
-for coord in boundary_coords:
-    boundary_fill(coord[0], coord[1], fill_color, boundary_color)
+# Streamlit UI
+st.title("Boundary Fill Algorithm")
+st.write("Click on any cell to fill with the selected color.")
+fill_color = st.color_picker("Select fill color", "#f00")
+boundary_color = st.color_picker("Select boundary color", "#000")
 
-# Plot the final 2D array
 fig, ax = plt.subplots()
-ax.imshow(two_d_arr, cmap='binary', interpolation='nearest')
-ax.set_xticks(np.arange(-0.5, 10, 1))
-ax.set_yticks(np.arange(-0.5, 10, 1))
-ax.grid(color='black', linestyle='-', linewidth=1)
-plt.show()
 
-# Display the plot in Streamlit
+# plot the initial array
+ax.imshow(two_d_arr, interpolation='none', cmap='gray_r')
+
+# event handler for mouse click
+def onclick(event):
+    if event.inaxes:
+        x, y = int(event.xdata), int(event.ydata)
+        boundary_fill(x, y, fill_color, boundary_color)
+        ax.imshow(two_d_arr, interpolation='none', cmap='gray_r')
+        fig.canvas.draw_idle()
+
+# attach the event handler to the plot
+cid = fig.canvas.mpl_connect('button_press_event', onclick)
+
 st.pyplot(fig)
