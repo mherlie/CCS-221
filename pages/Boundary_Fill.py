1e9fcc93dc3a75e79c3dc45ab552e309cfaf71b8
Mherlie Joy <126627774+mherlie@users.noreply.github.com>
2023-04-24 23:54:19 +0800

    Update Boundary_Fill.py

--- pages/Boundary_Fill.py
+++ pages/Boundary_Fill.py
@@ -2,38 +2,51 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
-# initialize the 2D array
-two_d_arr = np.zeros((10,10))
+# Define a 2D array as an image
+image = np.array([
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1]
+])
 
-# function for boundary fill algorithm
-def boundary_fill(x, y, fill_color, boundary_color):
-    if two_d_arr[x][y] != boundary_color and two_d_arr[x][y] != fill_color:
-        two_d_arr[x][y] = fill_color
-        boundary_fill(x-1, y, fill_color, boundary_color)
-        boundary_fill(x+1, y, fill_color, boundary_color)
-        boundary_fill(x, y-1, fill_color, boundary_color)
-        boundary_fill(x, y+1, fill_color, boundary_color)
+# Define the boundary fill function
+def boundary_fill(image, x, y, fill_color, boundary_color):
+    if image[x][y] != boundary_color and image[x][y] != fill_color:
+        image[x][y] = fill_color
+        if x > 0:
+            boundary_fill(image, x-1, y, fill_color, boundary_color)
+        if x < len(image)-1:
+            boundary_fill(image, x+1, y, fill_color, boundary_color)
+        if y > 0:
+            boundary_fill(image, x, y-1, fill_color, boundary_color)
+        if y < len(image[0])-1:
+            boundary_fill(image, x, y+1, fill_color, boundary_color)
 
-# Streamlit UI
-st.title("Boundary Fill Algorithm")
-st.write("Click on any cell to fill with the selected color.")
-fill_color = st.color_picker("Select fill color", "#f00")
-boundary_color = st.color_picker("Select boundary color", "#000")
+# Define the main function to run the app
+def main():
+    # Create a sidebar for input values
+    st.sidebar.title("Boundary Fill Algorithm")
+    col, row = st.sidebar.slider("Select a pixel to fill:", 0, len(image)-1, (2, 2))
+    fill_color = st.sidebar.slider("Fill color:", 0, 1, 1)
+    boundary_color = st.sidebar.slider("Boundary color:", 0, 1, 0)
 
-fig, ax = plt.subplots()
+    # Call the boundary fill function with the input values
+    boundary_fill(image, row, col, fill_color, boundary_color)
 
-# plot the initial array
-ax.imshow(two_d_arr, interpolation='none', cmap='gray_r')
+    # Display the image using matplotlib
+    fig, ax = plt.subplots()
+    ax.imshow(image, cmap='gray_r', interpolation='none')
+    ax.set_xticks(np.arange(-.5, len(image[0]), 1), minor=True)
+    ax.set_yticks(np.arange(-.5, len(image), 1), minor=True)
+    ax.grid(which='minor', color='w', linestyle='-', linewidth=2)
+    plt.title("Boundary Fill Algorithm")
+    plt.xlabel("X-axis")
+    plt.ylabel("Y-axis")
 
-# event handler for mouse click
-def onclick(event):
-    if event.inaxes:
-        x, y = int(event.xdata), int(event.ydata)
-        boundary_fill(x, y, fill_color, boundary_color)
-        ax.imshow(two_d_arr, interpolation='none', cmap='gray_r')
-        fig.canvas.draw_idle()
+    # Display the plot using streamlit
+    st.pyplot(fig)
 
-# attach the event handler to the plot
-cid = fig.canvas.mpl_connect('button_press_event', onclick)
-
-st.pyplot(fig)
+if __name__ == '__main__':
+    main()
