63105a2a56423e694e7335d12d8df38a93523e86
Mherlie Joy <126627774+mherlie@users.noreply.github.com>
2023-04-24 22:33:25 +0800

    Update ddaline.py

--- pages/ddaline.py
+++ pages/ddaline.py
@@ -1,16 +1,15 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 
+# DDALine algorithm
+st.subheader("DDA Line")
+st.sidebar.subheader("DDA Line Values")
+x1_DDALine, x2_DDALine, y1_DDALine, y2_DDALine = st.sidebar.slider('x1 for DDALine', 0, 15, 1),\
+                                                st.sidebar.slider('x2 for DDALine', 0, 15, 3),\
+                                                st.sidebar.slider('y1 for DDALine', 0, 15, 7),\
+                                                st.sidebar.slider('y2 for DDALine', 0, 15, 9)
 
-#DDA_LINE algorithm
-st.subheader("DDA-LINE")
-st.sidebar.subheader("DDA-LINE values")
-x1_DDALINE, x2_DDALINE, y1_DDALINE, y2_DDALINE = st.sidebar.slider('x1 for DDALINE', 0, 15, 1),\
-                                                st.sidebar.slider('x2 for DDALINE', 0, 15, 3),\
-                                                st.sidebar.slider('y1 for DDALINE', 0, 15, 7),\
-                                                st.sidebar.slider('y2 for DDALINE', 0, 15, 9)
-
-fig3 = plt.figure()
+fig3, ax = plt.subplots()
 def DDALine (x1, y1, x2, y2, color):
    
     dx = x2 - x1
@@ -20,7 +19,6 @@ def DDALine (x1, y1, x2, y2, color):
     yMidDDA = (y1 + y2)/2
 
     # calculate steps required for generating pixels
-
     steps = abs(dx) if abs(dx) > abs(dy) else abs(dy)
 
     # calculate increment in x and y for each step
@@ -31,9 +29,9 @@ def DDALine (x1, y1, x2, y2, color):
         #Draw pixels 
         x1 += Xinc
         y1 += Yinc
-        plt.plot(int(x1), int(y1), color)
-        plt.plot(int(xMidDDA), int(yMidDDA), 'b.')
+        ax.plot(int(x1), int(y1), color)
+        ax.plot(int(xMidDDA), int(yMidDDA), 'b.')
     return fig3
 
-fig3 = DDALine (x1_DDALINE, x2_DDALINE, y1_DDALINE, y2_DDALINE, 'ro')
+fig3 = DDALine(x1_DDALine, y1_DDALine, x2_DDALine, y2_DDALine, 'ro')
 st.pyplot(fig3)
