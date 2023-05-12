f31695cb8d03a216bd93c66fcc1cd40dfc6c121a
Mherlie Joy <126627774+mherlie@users.noreply.github.com>
2023-05-12 16:49:12 +0800

    Update IIID_Objects.py

--- pages/IIID_Objects.py
+++ pages/IIID_Objects.py
@@ -1,29 +1,15 @@
-import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
+
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from scipy.spatial import Delaunay
-import tensorflow as tf
+import streamlit as st
 
+import tensorflow as tf
 tf.compat.v1.disable_eager_execution()
 
-@st.cache
-def _plt_basic_object_(points):
-    tri = Delaunay(points).convex_hull
-    fig = plt.figure(figsize=(8, 8))
-    ax = fig.add_subplot(111, projection='3d')
-    S = ax.plot_trisurf(
-        points[:, 0], points[:, 1], points[:, 2],
-        triangles=tri,
-        shade=True, cmap=cm.rainbow, lw=0.5
-    )
-    ax.set_xlim3d(-5, 5)
-    ax.set_ylim3d(-5, 5)
-    ax.set_zlim3d(-5, 5)
-    return fig
 
-@st.cache
 def rotate_obj(points, angle):
     rotation_matrix = tf.stack([
         [tf.cos(angle), tf.sin(angle), 0],
@@ -31,55 +17,128 @@ def rotate_obj(points, angle):
         [0, 0, 1]
     ])
     rotate_object = tf.matmul(tf.cast(points, tf.float32), tf.cast(rotation_matrix, tf.float32))
-    with tf.compat.v1.Session() as session:
-        rotated_object = session.run(rotate_object)
-    return rotated_object
 
-def translate_obj(points, amount):
-    return tf.add(points, amount)
+    return rotate_object
+
+
+def translate_obj(points, translation):
+    translation_vector = tf.constant(translation, dtype=tf.float32)
+    translated_object = tf.add(tf.cast(points, tf.float32), translation_vector)
+
+    return translated_object
+
+
+def _plt_basic_object_(points):
+    tri = Delaunay(points).convex_hull
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111, projection='3d')
+    S = ax.plot_trisurf(points[:, 0], points[:, 1], points[:, 2],
+                        triangles=tri,
+                        shade=True, cmap=cm.rainbow, lw=0.5)
+
+    ax.set_xlim3d(-5, 5) 
+    ax.set_ylim3d(-5, 5)
+    ax.set_zlim3d(-5, 5) 
+
+
+def _rectangle(bottom_lower=(0, 0, 0), side_length=5, length=-4):
+    bottom_lower = np.array(bottom_lower)
+
+    points = np.vstack([
+        bottom_lower,
+        bottom_lower + [0, side_length, 0],
+        bottom_lower + [length, side_length, 0],
+        bottom_lower + [length, 0, 0],
+        bottom_lower + [0, 0, side_length],
+        bottom_lower + [0, side_length, side_length],
+        bottom_lower + [length, side_length, side_length],
+        bottom_lower + [length, 0, side_length],
+        bottom_lower,
+    ])
+
+    return points
+
+
+def right_tri(bottom_lower=(0, 0, 0), side_length=3):
+    
+    bottom_lower = np.array(bottom_lower)
+
+    points = np.vstack([
+        bottom_lower,
+        bottom_lower + [0, side_length, 0],
+        bottom_lower + [side_length, side_length, 0],
+        bottom_lower + [0, 0, side_length],
+        bottom_lower + [0, side_length, side_length],
+        bottom_lower + [side_length, 0, 0],
+        bottom_lower + [side_length, 0, 0],
+        bottom_lower + [side_length, 0, 0],
+        bottom_lower + [0, 0, side_length],
+        bottom_lower,
+    ])
+
+    return points
+
+
+def tri_prism(bottom_lower=(0, 0, 0), side_length=5, side=4, two=2):
+    bottom_lower = np.array(bottom_lower)
+
+    points = np.vstack([
+        bottom_lower,
+        bottom_lower + [0, side, 0],
+        bottom_lower + [side, side, 0], 
+        bottom_lower + [side, 0, 0], 
+        bottom_lower + [two, side, side_length],
+        bottom_lower + [two, side, side_length],
+        bottom_lower + [two, side, side_length],
+        bottom_lower + [two, 0, side_length],
+        bottom_lower + [two, 0, side_length],
+        bottom_lower,
+    ])
+
+    return points
+
 
 def main():
-    st.title("3D Object Manipulation")
+    st.title("3D Object Transformation")
+    st.sidebar.header("Image Transformations")
+    object_types = ["Rectangle", "Right Triangle", "Triangular Prism"]
+    object_type = st.sidebar.selectbox("Select Object Type", object_types)
 
-    object_type = st.selectbox("Select an object type", ("Cube", "Rectangle", "Right Triangle", "Triangle Prism"))
-
-    if object_type == "Cube":
-        init_cube = _cube_(side_length=3)
-        points = tf.constant(init_cube, dtype=tf.float32)
-
-    elif object_type == "Rectangle":
-        init_rectangle = rectangle_obj(side_length=3)
-        points = tf.constant(init_rectangle, dtype=tf.float32)
-
-    elif object_type == "Right Triangle":
-        init_right_triangle = right_triangle_obj(side_length=5)
-        points = tf.constant(init_right_triangle, dtype=tf.float32)
-
+    transformation_types = ["Rotate", "Translate"]
+    transformation_type = st.sidebar.selectbox("Select Transformation Type", transformation_types)
+    
+    if transformation_type == "Rotate":
+        angle = st.sidebar.slider("Rotation Angle", -360.0, 360.0, step=1.0)
     else:
-        init_tri_prism = triangle_prism_obj(side_length=4)
-        points = tf.constant(init_tri_prism, dtype=tf.float32)
+        angle = (0.0)
 
-    st.subheader("Original Object")
-    fig = _plt_basic_object_(points)
-    st.pyplot(fig)
+    if transformation_type == "Translate":
+        translation_x = st.sidebar.slider("Translation X", -5.0, 5.0, step=0.1, value=0.0)
+        translation_y = st.sidebar.slider("Translation Y", -5.0, 5.0, step=0.1, value=0.0)
+        translation_z = st.sidebar.slider("Translation Z", -5.0, 5.0, step=0.1, value=0.0)
+        translation = (translation_x, translation_y, translation_z)
+    else:
+        translation = (0.0, 0.0, 0.0)
 
-    translate_amount = st.text_input("Enter translation amount (e.g., 1,2,2)")
-    translate_amount = np.array([float(x) for x in translate_amount.split(",")])
+    if object_type == "Rectangle":
+        init_object = _rectangle(side_length=5, length=-4)
+    elif object_type == "Right Triangle":
+        init_object = right_tri(side_length=3)
+    elif object_type == "Triangular Prism":
+        init_object = tri_prism(side_length=5, side=4, two=2)
 
-    translated_object = translate_obj(points, tf.constant(translate_amount, dtype=tf.float32))
+    points = tf.constant(init_object, dtype=tf.float32)
+
     with tf.compat.v1.Session() as session:
-        translated_object = session.run(translated_object)
+        if transformation_type == "Rotate":
+            transformed_object = session.run(rotate_obj(points, np.radians(angle)))
+        elif transformation_type == "Translate":
+            transformed_object = session.run(translate_obj(points, translation))
 
-    st.subheader("Translated Object")
-    fig = _plt_basic_object_(translated_object)
-    st.pyplot(fig)
+    _plt_basic_object_(transformed_object)
+    st.pyplot(plt)
 
-    rotate_angle = st.number_input("Enter rotation angle", value=75.0)
-    rotated_object = rotate_obj(points, rotate_angle)
-
-    st.subheader("Rotated Object")
-    fig = _plt_basic_object_(rotated_object)
-    st.pyplot(fig)
 
 if __name__ == "__main__":
     main()
